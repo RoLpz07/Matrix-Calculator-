89374f7187d7bc7099232dcfb38784b73a2225d8
RoLpz07 <ronaldchino0708@gmail.com>
2025-06-09 23:59:36 -0400

    Implement matrix addition and subtraction functions

--- src/js/matrix.js
+++ src/js/matrix.js
@@ -1,1 +1,39 @@
 console.log('Matrix Calculator listo.');
+
+
+function sumarMatrices(A, B) {
+    if (!sonDimensionesIguales(A, B)) {
+        throw new Error('Las matrices deben tener las mismas dimensiones para sumarse.');
+    }
+    return A.map((fila, i) => fila.map((val, j) => val + B[i][j]));
+}
+
+
+function restarMatrices(A, B) {
+    if (!sonDimensionesIguales(A, B)) {
+        throw new Error('Las matrices deben tener las mismas dimensiones para restarse.');
+    }
+    return A.map((fila, i) => fila.map((val, j) => val - B[i][j]));
+}
+
+
+function sonDimensionesIguales(A, B) {
+    return (
+        Array.isArray(A) && Array.isArray(B) &&
+        A.length === B.length &&
+        A.every((fila, i) => Array.isArray(fila) && Array.isArray(B[i]) && fila.length === B[i].length)
+    );
+}
+
+
+const matrizA = [
+    [1, 2],
+    [3, 4]
+];
+const matrizB = [
+    [5, 6],
+    [7, 8]
+];
+
+console.log('Suma:', sumarMatrices(matrizA, matrizB));
+console.log('Resta:', restarMatrices(matrizA, matrizB));
